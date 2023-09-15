c70d3fdcc639e3f23a99b1b0de35e23a07ada4e7
julysNICK <julystube@gmail.com>
2023-09-15 14:43:36 -0300

    add cards boxes

---
+++ src/components/CardBox.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+
+defineProps({
+  title: String,
+  icon: String,
+  // number: String,
+  color: String
+})
+
+
+</script>
+
+<template>
+  <div class="box" :class="color">
+    <i class="uil" :class="icon"></i>
+    <span class="text">
+      {{ title }}
+    </span>
+    <span class="number">
+      50,120
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.dashboard-content .boxes .box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 10px;
+  width: calc(100% / 3 - 15px);
+  padding: 15px 20px;
+  background-color: var(--box1-color);
+}
+
+.boxes .box i {
+  font-size: 35px;
+  color: var(--text-color)
+}
+
+.boxes .box .text {
+  white-space: nowrap;
+  font-size: 18px;
+  font-weight: 500;
+  color: var(--text-color);
+  margin-top: 10px;
+}
+
+.boxes .box .number {
+  white-space: nowrap;
+  font-size: 40px;
+  font-weight: 500;
+  color: var(--text-color);
+  margin-top: 10px;
+}
+
+.boxes .box.box2 {
+  background-color: var(--box2-color);
+}
+
+.boxes .box.box3 {
+  background-color: var(--box3-color);
+}
+</style>
---
+++ src/components/Overview.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import CardBox from './CardBox.vue';
+
+</script>
+
+<template>
+  <div class="overview">
+    <div class="title">
+      <i class="uil uil-tachometer-fast"></i>
+      <span class="text">
+        Dashboard
+      </span>
+    </div>
+    <div class="boxes">
+      <CardBox color="box1" title="Total Likes" icon="uil-thumbs-up" />
+      <CardBox color="box2" title="Total Comments" icon="uil-comment-alt" />
+      <CardBox color="box3" title="Total shares" icon="uil-share" />
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.dashboard .dashboard-content {
+  padding-top: 50px;
+}
+
+.dashboard-content .title {
+  display: flex;
+  align-items: center;
+  margin: 60px 0 30px 0;
+}
+
+.dashboard-content .title i {
+  position: relative;
+  height: 35px;
+  width: 35px;
+  background-color: var(--primary-color);
+  border-radius: 50%;
+  color: var(--title-icon-color);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+}
+
+.dashboard-content .title .text {
+  font-size: 24px;
+  font-weight: 500;
+  margin-left: 10px;
+  color: var(--text-color);
+}
+
+.dashboard-content .boxes {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
